25e0c0e1d6db520f08c24fbc1d3010e34743915f
Daniel Stutzman <dtstutz@gmail.com>
2018-05-22 14:30:54 -0600

    Output .class to args[1]

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 * mvn test
 * mvn compile
-* mvn exec:java -Dexec.args="fixtures/input/hello.kt"
+* mvn exec:java -Dexec.args="fixtures/input/hello.kt fixtures/output-actual/HelloKt.class"
--- src/main/kotlin/Kotlinc.kt
+++ src/main/kotlin/Kotlinc.kt
@@ -9,26 +9,35 @@ import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
 fun main(args: Array<String>) {
-  if (args.size == 0) {
-    System.err.println("Usage: First arg should be path to .kt file")
+  if (args.size != 2) {
+    System.err.println("""Usage:
+      Arg 1: path to .kt input
+      Arg 2: path to .class output
+      """.trimIndent())
     System.exit(1)
   }
-  val sourcePath = File(args[0])
+  val inputPath = File(args[0])
+  val outputPath = File(args[1])
 
-  val kotlinLexer = KotlinLexer(CharStreams.fromString(sourcePath.readText()))
+  val kotlinLexer = KotlinLexer(CharStreams.fromString(inputPath.readText()))
   val commonTokenStream = CommonTokenStream(kotlinLexer)
   val kotlinParser = KotlinParser(commonTokenStream)
   val tree = kotlinParser.kotlinFile()
   val visitor = ToAstVisitor()
   val fileContents = visitor.visit(tree) as FileContents
-  val resolved = resolveClass("HelloKt", fileContents)
+  val className = filenameToClassName(inputPath.getName())
+  val resolved = resolveClass(className, fileContents)
   val bytecode = flattenClass(resolved)
 
-  DataOutputStream(FileOutputStream(File("HelloKt.class"))).use { stream ->
+  DataOutputStream(FileOutputStream(outputPath)).use { stream ->
     bytecode.write(stream)
   }
-  println("HelloKt.class")
+  println(outputPath)
 }
+
+fun filenameToClassName(filename: String): String =
+  filename.substring(0, 1).toUpperCase() +
+  filename.substring(1).replace("\\.kt$", "Kt")
 
 fun resolveClass(className: String, fileContents: FileContents): Nested.Class {
   val constructor_ = Nested.Method(
